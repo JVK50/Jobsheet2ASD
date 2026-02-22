d9b681bfb288eef9c05187840dddbd2acc069500
JVK50 <jidanali32@gmail.com>
2026-02-22 20:15:15 +0700

    commit

--- mahasiswa13.java
+++ mahasiswa13.java
@@ -1,6 +1,37 @@
 public class mahasiswa13 {
+String nama;
+String nim;
+String kelas;
+double ipk;
 
-    public static void main(String[] args) {
-        
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIPK(double ipkBaru) {
+        if (ipk >= 0.0 && ipk <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.5) {
+            return "Kinerja cukup";
+        } else if (ipk >= 2.0) {
+            return "Kinerja kurang";
+        }
     }
 }
